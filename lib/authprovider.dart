0bd10b52a723a4cfdbddc271200600dde9496669
Mark Karenin <adamoroff@gmail.com>
2022-07-21 23:45:08 +0300

    🔨 Moved env vars to other file

--- lib/authprovider.dart
+++ lib/authprovider.dart
@@ -4,9 +4,7 @@ import 'package:bronesystem/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart';
-
-const baseUrl = "http://10.0.2.2:8000";
-const loginUrl = "$baseUrl/api/login/";
+import 'vars.dart';
 
 enum Status { notLoggedIn, authenticating, loggedIn }
 
@@ -18,7 +16,10 @@ class AuthProvider extends ChangeNotifier {
   Status get status => _loggedInStatus;
 
   login(String name, int apartment) async {
-    final Map<String, dynamic> loginData = {'name': name, 'apartment': apartment};
+    final Map<String, dynamic> loginData = {
+      'name': name,
+      'apartment': apartment
+    };
 
     post(
       Uri.parse(loginUrl),
@@ -31,17 +32,14 @@ class AuthProvider extends ChangeNotifier {
         _loggedInStatus = Status.authenticating;
         notifyListeners();
         _saveSession(user!);
-      }
-      else {
+      } else {
         print(response.statusCode);
         _loggedInStatus = Status.notLoggedIn;
         notifyListeners();
       }
-    })
-    .catchError((onError) {
+    }).catchError((onError) {
       print(onError);
     });
-
   }
 
   void _saveSession(User user) async {
---
+++ lib/vars.dart
@@ -0,0 +1,6 @@
+const baseUrl = "http://10.0.2.2:8000";
+const loginUrl = "$baseUrl/api/login/";
+const servicesUrl = "$baseUrl/api/services/";
+Uri baseUri = Uri.parse(baseUrl);
+Uri loginUri = Uri.parse(loginUrl);
+Uri servicesUri = Uri.parse(servicesUrl);
